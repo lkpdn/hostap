8b9e9c6b2540a34626ff2dcf7576a7317c4e1671
lkpdn <den.not.denmark@gmail.com>
2018-03-01 23:52:01 +0900

    PAE: add common 802.1X header

---
+++ src/pae/ieee802_1x_pae.h
@@ -0,0 +1,16 @@
+/*
+ * IEEE 802.1X-2010 PAE
+ * Copyright (c) 2013, Qualcomm Atheros, Inc.
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+#ifndef IEEE802_1X_PAE_H
+#define IEEE802_1X_PAE_H
+
+struct ieee802_1x_peer_pae {
+	Boolean soliciting_announcement;
+};
+
+#endif /* IEEE802_1X_PAE_H */
