b2db455bc17efa4f04a2c1585f8c7139d19a8064
lkpdn <den.not.denmark@gmail.com>
2018-04-19 21:14:32 +0900

    AP: turn on generic PAE/Logon implementations

--- src/pae/ieee802_1x_pae.c
+++ src/pae/ieee802_1x_pae.c
@@ -6,11 +6,14 @@
  * See README for more details.
  */
 
-#include <time.h>
-#include "includes.h"
-#include "common.h"
-#include "list.h"
-#include "eloop.h"
+#include "utils/includes.h"
+#include "utils/common.h"
+#include "utils/eloop.h"
+
+#include "common/defs.h"
+#include "common/ieee802_1x_defs.h"
+#include "common/eapol_common.h"
+#include "ieee802_1x_pae.h"
 
 
 #define ANNOUNCEMENT_TIME 5000
@@ -59,35 +62,37 @@ ieee802_1x_pae_validate_announcement(int tlv_type, int packet_type, int global)
 /**
  * ieee802_1x_pae_decode_announcement -
  */
-static void ieee802_1x_decode_announcement(u8 *ann, size_t ann_len, int packet_type)
+void ieee802_1x_decode_announcement(u8 *ann, size_t ann_len, int packet_type,
+				    const ieee802_1x_announcement_handler *handlers)
 {
-       struct ieee802_1x_ann_tlv_hdr *hdr;
-       u8 *pos;
-       size_t left, len;
-       int type, global = 1;
+	struct ieee802_1x_ann_tlv_hdr *hdr;
+	u8 *pos;
+	size_t left, len;
+	int type, global = 1;
 
-       pos = ann;
-       left = ann_len;
+	pos = ann;
+	left = ann_len;
 
-       while (left > sizeof(*hdr)) {
-	       hdr = (struct ieee802_1x_ann_tlv_hdr *) pos;
-	       type = be_to_host16(hdr->type);
-	       len = be_to_host16(hdr->len);
-	       pos += sizeof(*hdr);
-	       left -= sizeof(*hdr);
-	       if (len > left) {
-		       wpa_printf(MSG_DEBUG, "IEEE 802.1X: EAPOL-Announcement "
-				  "TLV overrun (type=%d len=%lu left=%lu)",
-				  type, (unsigned long) len,
-				  (unsigned long) left);
-	       }
+	while (left > sizeof(*hdr)) {
+		hdr = (struct ieee802_1x_ann_tlv_hdr *) pos;
+		type = be_to_host16(hdr->type);
+		len = be_to_host16(hdr->len);
+		pos += sizeof(*hdr);
+		left -= sizeof(*hdr);
+		if (len > left) {
+			wpa_printf(MSG_DEBUG, "IEEE 802.1X: EAPOL-Announcement "
+			   "TLV overrun (type=%d len=%lu left=%lu)",
+			   type, (unsigned long) len,
+			   (unsigned long) left);
+		}
 
-	       if (ieee802_1x_pae_validate_announcement(type, packet_type, global)) {
-		       ieee802_1x_parse_ann_tlv(type, pos, len, &global);
-	       }
+		if (ieee802_1x_pae_validate_announcement(type, packet_type,
+							 global)) {
+			handlers[type & 0x7f](NULL, packet_type, &global);
+		}
 
-	       pos += len;
-	       left -= len;
+		pos += len;
+		left -= len;
        }
 }
 
@@ -95,45 +100,30 @@ static void ieee802_1x_decode_announcement(u8 *ann, size_t ann_len, int packet_t
 /**
  * ieee802_1x_pae_encode_announcement_generic -
  */
-static int
-ieee802_1x_pae_encode_announcement_generic(struct hostapd_data *hapd,
-					   struct wpabuf *pbuf)
+int ieee802_1x_pae_encode_announcement_generic(u8 *own_addr,
+					       struct wpabuf *pbuf)
 {
+	struct ieee8023_hdr *ether_hdr;
+	struct ieee802_1x_hdr *eapol_hdr;
+	int i;
+
 	ether_hdr = wpabuf_put(pbuf, sizeof(*ether_hdr));
 	os_memcpy(ether_hdr->dest, pae_group_addr, sizeof(ether_hdr->dest));
-	os_memcpy(ether_hdr->src, hapd->own_addr);
+	os_memcpy(ether_hdr->src, own_addr, sizeof(ether_hdr->src));
 	ether_hdr->ethertype = host_to_be16(ETH_P_EAPOL);
 
-	eapol_hdr = wpabuf_put(pbuf, sizeof(*eapol_hdr);
+	eapol_hdr = wpabuf_put(pbuf, sizeof(*eapol_hdr));
 	eapol_hdr->version = EAPOL_VERSION;
 	eapol_hdr->type = IEEE802_1X_TYPE_EAPOL_ANNOUNCEMENT_GENERIC;
 
-	for (i = 0; i < ARRAY_SIZE(announcement_tlvs); i++) {
-		/* unimplemented */
-	}
+	/* unimplemented */
 }
 
 
 /**
  * ieee802_1x_pae_xmit_announcement -
  */
-static void ieee802_1x_xmit_announcement(struct hostapd_data *hapd)
+void ieee802_1x_xmit_announcement(void *priv)
 {
 	/* unimplemented */
-}
-
-
-/**
- * ieee802_1x_announcement_timer -
- */
-static void ieee802_1x_announcement_timer(void *eloop_ctx, void *timeout_ctx)
-{
-	struct hostapd_data *hapd;
-
-	hapd = (struct hostapd_data *)eloop_ctx;
-	ieee802_1x_xmit_announcement(hapd);
-	eloop_register_timeout(ANNOUNCEMENT_TIME / 1000, 0,
-			       ieee802_1x_announcement_timer,
-			       hapd, NULL);
-	return;
 }
--- src/pae/ieee802_1x_pae.h
+++ src/pae/ieee802_1x_pae.h
@@ -1,6 +1,6 @@
 /*
- * IEEE 802.1X-2010 PAE
- * Copyright (c) 2013, Qualcomm Atheros, Inc.
+ * IEEE 802.1X-2010 PAE/Logon Process
+ * Copyright (c) 2018, Qualcomm Atheros, Inc.
  *
  * This software may be distributed under the terms of the BSD license.
  * See README for more details.
@@ -9,8 +9,22 @@
 #ifndef IEEE802_1X_PAE_H
 #define IEEE802_1X_PAE_H
 
+#include "includes.h"
+#include "common.h"
+#include "list.h"
+
+#define ANNOUNCEMENT_TIME 5000
+
+typedef int (*ieee802_1x_announcement_handler)(void *priv, int packet_type,
+					       int *global);
+
 struct ieee802_1x_peer_pae {
-	Boolean soliciting_announcement;
+       Boolean soliciting_announcement;
 };
+
+void ieee802_1x_decode_announcement(u8 *ann, size_t ann_len, int packet_type,
+				    const ieee802_1x_announcement_handler *handlers);
+int ieee802_1x_pae_encode_announcement_generic(u8 *own_addr, struct wpabuf *pbuf);
+void ieee802_1x_xmit_announcement(void *priv);
 
 #endif /* IEEE802_1X_PAE_H */
