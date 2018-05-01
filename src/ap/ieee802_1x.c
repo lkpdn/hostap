458e952b1945f295ab31dcce0d61f592d93b94c2
lkpdn <den.not.denmark@gmail.com>
2018-05-01 22:06:23 +0900

    AP: turn on specific announcement encoding

--- src/ap/ieee802_1x.c
+++ src/ap/ieee802_1x.c
@@ -898,22 +898,46 @@ static void ieee802_1x_save_eapol(struct sta_info *sta, const u8 *buf,
 static void
 ieee802_1x_generic_announcement_timeout(void *eloop_ctx, void *timeout_ctx)
 {
-	struct sta_info *sta = eloop_ctx;
-	u8 announcement = 0;
+	struct sta_info *sta;
+	struct wpabuf *buf;
+	size_t length = 0;
 
-	/* XXX: currently wired drivers set PAE packet destination address
-	 *      based on config parameter use_pae_group_address, however
-	 *      we need to send both to group address and individual STA
-	 *      address depending on announcement types defined in .1X-2010
-	 */
-	sta->eapol_sm->eapol->cb.eapol_send(
-		sta->eapol_sm->eapol->conf.ctx, sta,
-		IEEE802_1X_TYPE_EAPOL_ANNOUNCEMENT_GENERIC,
-		(u8 *) &announcement, sizeof(announcement));
+	if (eloop_ctx) {
+		/* Specific announcement
+		 *
+		 * XXX: currently wired drivers set PAE packet destination address
+		 *      based on config parameter use_pae_group_address, however
+		 *      we need to send both to group address and individual STA
+		 *      address depending on announcement types defined in .1X-2010
+		 */
+		sta = eloop_ctx;
+		length = ieee802_1x_pae_len_announcement_generic(
+			handlers, NULL, own_addr, buf, sta);
 
-	eloop_register_timeout(IEEE8021X_ANN_TIME / 1000, 0,
-			       ieee802_1x_generic_announcement_timeout,
-			       sta, NULL);
+		buf = wpabuf_alloc(length);
+		if (!buf) {
+			wpa_printf(MSF_ERROR, "AP: out of memory");
+			return;
+		}
+
+		ieee802_1x_pae_encode_announcement_generic(
+			handlers, NULL, own_addr, buf, sta);
+
+		sta->eapol_sm->eapol->cb.eapol_send(
+			sta->eapol_sm->eapol->conf.ctx, sta,
+			IEEE802_1X_TYPE_EAPOL_ANNOUNCEMENT_GENERIC,
+			wpabuf_head(buf), wpabuf_len(buf));
+
+	} else {
+		/* Generic announcement
+		 *
+		 * _unimplemented_
+		 */
+		u8 announcement = 0;
+		eloop_register_timeout(IEEE8021X_ANN_TIME / 1000, 0,
+				       ieee802_1x_generic_announcement_timeout,
+				       sta, NULL);
+	}
 }
 
 
